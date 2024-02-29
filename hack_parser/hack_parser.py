9e40daa6c90c9c107503a6d5538545fd71690216
PE51K <grigorij1m@gmail.com>
2024-02-29 17:40:48 +0300

    Новая заглушка под парсер

--- hack_parser/hack_parser.py
+++ hack_parser/hack_parser.py
@@ -1,13 +1,20 @@
 class HackParser:
-    def __init__(self, file_path):
-        self.file_text = ...
+    def __init__(self, file_path=None):
+        if not file_path:
+            self.text = ...
+        else:
+            self.text = ...
+        self.model = ...
+
+    def set_file(self, file_path):
+        self.text = ...
 
     def get_name(self):
-        return self.file_text
+        return self.model()
 
     def parse(self):
         return {
-            "first_name": self.get_name(),
+            "first_name": self.get_name(file_text),
             "last_name": "",
             "middle_name": "",
             "birth_date": "",
