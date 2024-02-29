c9066c366cd26bbe31c668d0d27c06f7b14e3754
AlexStr94 <alex.strunskiy@ya.ru>
2024-02-29 18:04:02 +0300

    first iteration

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ML_TalentMatch
+
+Запуск проекта:
+
+```
+sudo docker build --no-cache -t resume_parser .
+sudo docker run -d --name resume_parser -p 80:80 resume_parser
+
+```
---
+++ app/main.py
@@ -0,0 +1,55 @@
+import uvicorn
+from fastapi import FastAPI, HTTPException, status, UploadFile
+from fastapi.responses import JSONResponse
+
+
+class FileError(HTTPException):
+    def __init__(
+        self,
+        status_code: int = status.HTTP_400_BAD_REQUEST,
+        detail: str = 'Incorrect file',
+        headers: dict = {"WWW-Authenticate": "Bearer"}
+    ) -> None:
+        super().__init__(
+            status_code=status_code,
+            detail=detail,
+            headers=headers
+        )
+
+
+class FileTypeError(FileError):
+    def __init__(
+        self,
+        detail: str = 'Incorrect file type',
+    ) -> None:
+        super().__init__(
+            detail=detail,
+        )
+
+app = FastAPI()
+
+
+@app.post("/parse_resume/")
+async def parse_resume(
+    file_in: UploadFile,
+):
+    if not file_in.size:
+        raise FileError
+    if not (file_type := file_in.content_type):
+        raise FileTypeError  
+    if file_type not in (
+        'application/msword',
+        'application/pdf',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    ):
+        raise FileTypeError  
+    
+    return {"file_type": file_type}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        'main:app',
+        host='127.0.0.1',
+        port=8000,
+    )
